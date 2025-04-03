ad0bc248a12f36703d36520d3a5cb58f4e13436e
YnwCh3f <benacsekd07@gmail.com>
2025-04-03 20:44:05 +0200

    test v1

---
+++ index.test.js
@@ -0,0 +1,159 @@
+import { describe, test, expect } from "vitest";
+
+
+//  GET
+
+describe('GET /foods', () => {
+    test("", () =>{
+
+    })
+});
+
+describe('GET /users', () => {
+    test("", () =>{
+
+    })
+});
+describe('GET /nutritions', () => {
+    test("", () =>{
+
+    })
+});
+describe('GET /chat/:sender_id/:recipient_id', () => {
+    test("", () =>{
+
+    })
+});
+describe('GET /login', () => {
+    test("", () =>{
+
+    })
+});
+describe('GET /cart', () => {
+    test("", () =>{
+
+    })
+});
+describe('GET /restaurants', () => {
+    test("", () =>{
+
+    })
+});
+
+
+//  POST
+
+
+describe('POST /food', () => {
+    test("", () =>{
+
+    })
+});
+
+describe('POST /user', () => {
+    test("", () =>{
+
+    })
+});
+describe('POST /nutrition', () => {
+    test("", () =>{
+
+    })
+});
+describe('POST /message', () => {
+    test("", () =>{
+
+    })
+});
+describe('POST /addtocart', () => {
+    test("", () =>{
+
+    })
+});
+describe('POST /restaurant', () => {
+    test("", () =>{
+
+    })
+});
+
+
+//  DELETE
+
+
+describe('DELETE /food/:id', () => {
+    test("", () =>{
+
+    })
+});
+
+describe('DELETE /user/:id', () => {
+    test("", () =>{
+
+    })
+});
+describe('DELETE /nutrition/:id', () => {
+    test("", () =>{
+
+    })
+});
+describe('DELETE /delfromcart/:user_id/:food_id', () => {
+    test("", () =>{
+
+    })
+});
+describe('DELETE /clearcart/:user_id', () => {
+    test("", () =>{
+
+    })
+});
+describe('DELETE /restaurant/:id', () => {
+    test("", () =>{
+
+    })
+});
+
+
+// PUT
+
+
+describe('PUT /food/:id', () => {
+    test("", () =>{
+
+    })
+});
+
+describe('PUT /user/:id', () => {
+    test("", () =>{
+
+    })
+});
+describe('PUT /nutrition/:id', () => {
+    test("", () =>{
+
+    })
+});
+
+
+//  PATCH
+
+describe('PATCH /foodimage/:id', () => {
+    test("", () =>{
+
+    })
+});
+
+describe('PATCH /foodprice/:id', () => {
+    test("", () =>{
+
+    })
+});
+describe('PATCH /user/points/:id', () => {
+    test("", () =>{
+
+    })
+});
+describe('PATCH /message', () => {
+    test("", () =>{
+
+    })
+});
